d13b10a8a15842db017d03cbe737c5d43d16b996
rahu12t111 <96530286+rahu12t111@users.noreply.github.com>
2025-11-18 15:33:01 +0100

    Update GELU.py

--- BERT_Architecture/GELU.py
+++ BERT_Architecture/GELU.py
@@ -4,5 +4,7 @@ import matplotlib.pyplot as plt
 
 plt.plot(x, gelu, color='blue', label='GELU')
 plt.plot(x, relu, color='red', label='ReLU')
+plt.xlabel('X')
+plt.ylabel('Activation(X)')
 plt.savefig('GELU_activation.png', dpi=300, transparent=True)
 plt.show()
