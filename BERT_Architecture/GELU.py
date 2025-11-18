c4f1ca32c53d378eb5c0cbb07904c1ee4c0873ae
rahu12t111 <96530286+rahu12t111@users.noreply.github.com>
2025-11-18 15:25:02 +0100

    Create GELU.py

---
+++ BERT_Architecture/GELU.py
@@ -0,0 +1,8 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.plot(x, gelu, color='blue', label='GELU')
+plt.plot(x, relu, color='red', label='ReLU')
+plt.savefig('GELU_activation.png', dpi=300)
+plt.show()
